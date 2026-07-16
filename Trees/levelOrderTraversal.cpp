26c9a30366d04dde3898d1cf019c8ec284ae1e45
Praveen1116 <kpraveen1116@gmail.com>
2026-07-16 16:46:04 +0530

    Level Order Traversal

---
+++ Trees/levelOrderTraversal.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void levelOrder(Node* root)
+{
+    if (root == NULL)
+        return;
+
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty())
+    {
+        Node* frontNode = q.front();
+        q.pop();
+
+        if(frontNode != NULL)
+        {
+            cout << frontNode->data << " ";
+            if(frontNode->left) q.push(frontNode->left);
+            if(frontNode->right) q.push(frontNode->right);
+        }
+        else if(!q.empty())
+        {
+            cout << endl;
+            q.push(NULL);
+        }
+    }
+}
+
+int main()
+{
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    levelOrder(root);
+
+    return 0;
+}
